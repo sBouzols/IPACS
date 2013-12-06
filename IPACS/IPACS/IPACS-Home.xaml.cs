2bc324f96dc8431f18de8921daaef9da6f9186e8
sBouzols <sylvain.bouzols@gmail.com>
2013-12-06 17:44:15 +0100

    Add IPACS PRoject

---
+++ IPACS/IPACS/IPACS-Home.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IPACS
+{
+    /// <summary>
+    /// Interaction logic for IPACS_Home.xaml
+    /// </summary>
+    public partial class IPACS_Home : Page
+    {
+        public IPACS_Home()
+        {
+            InitializeComponent();
+        }
+
+        private void image2_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+    }
+}
